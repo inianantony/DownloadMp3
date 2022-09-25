4284341e4996b953ced0a3b8f78384d617738dc3
Inian Antony <inianantony.s@gmail.com>
2022-09-25 12:53:46 +0800

    add tamilmp3free download

--- DownloadMp3/Program.cs
+++ DownloadMp3/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using System.Transactions;
 
 namespace DownloadMp3
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var mp3Download = new Mp3Download();
-            mp3Download.ProcessDownload();
+            if (args != null && args.Length > 2)
+            {
+                var mp3Download = new Mp3Download();
+                mp3Download.ProcessDownload();
+                var tamilmp3FreeDownload = new Tamilmp3FreeDownload();
+                await tamilmp3FreeDownload.ProcessDownloadAsync();
+            }
+            
+
+
+
+
             Console.WriteLine("Download is Completed!");
             Console.Read();
         }
